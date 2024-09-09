e8a34599bb18c8138952121c65b00f3ac619d178
Jonathan Holmberg <127875373+JonathanOHolmberg@users.noreply.github.com>
2024-09-09 13:05:44 +0300

    adding in theme toggle to settings

--- site/src/components/Settings.tsx
+++ site/src/components/Settings.tsx
@@ -1,8 +1,22 @@
+import { useTheme } from "@/context/ThemeContext";
+import CheckToggle from "./components/CheckToggle";
+
+import { SunIcon } from "./icons/Sun";
+import { MoonIcon } from "./icons/Moon";
+
 const Settings: React.FC = () => {
+
+    const { theme } = useTheme()
     
     return (
         <div className="settings">
-            
+            <CheckToggle
+                value={theme.value}
+                setValue={theme.setValue}
+                defaultValue="light"
+                toggleValue="dark"
+                defaultIcon={<SunIcon />}
+                toggleIcon={<MoonIcon />} />
         </div>
     );
 }
---
+++ site/src/components/components/CheckToggle.tsx
@@ -0,0 +1,22 @@
+import { CheckToggleProps } from "@/config/types/ToggleProps";
+
+import "@/styles/components/Components/toggle.css"
+
+const CheckToggle: React.FC<CheckToggleProps> = ({ defaultValue, defaultIcon, toggleValue, toggleIcon, setValue, value }) => {
+
+    const changeValue = () => {
+        setValue(value === defaultValue ? toggleValue : defaultValue)
+    }
+
+    return (
+        <div className="toggle">
+          <label className="switch">
+            <input checked={value === toggleValue} type="checkbox" onChange={() => changeValue()}/>
+            <span className="slider"></span>
+          </label>
+          {value === defaultValue ? defaultIcon : toggleIcon}
+        </div>
+    );
+}
+
+export default CheckToggle;
